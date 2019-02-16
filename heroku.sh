2759f7eb25f609157c3cd9ad6fdac86d7c87d71c
shokohara <shokohara@users.noreply.github.com>
2019-02-17 05:21:49 +0900

    CIを設定する

--- heroku.sh
+++ heroku.sh
@@ -1,4 +1,7 @@
 set -eu
 sbt 'web/docker:publishLocal'
 docker tag web:0.1.0-SNAPSHOT registry.heroku.com/fierce-depths-21005/web
+heroku container:login
+docker push registry.heroku.com/fierce-depths-21005/web
+heroku container:push web -a fierce-depths-21005
 heroku container:release web -a fierce-depths-21005
