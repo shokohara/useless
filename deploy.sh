95afb9a093fb058d09ea642e8db8c402e49e7d2d
shokohara <shokohara@users.noreply.github.com>
2019-02-17 16:22:57 +0900

    デプロイスクリプトのファイル名を変更する

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,4 @@ sbt 'web/docker:publishLocal'
 docker tag web:0.1.0-SNAPSHOT registry.heroku.com/fierce-depths-21005/web
 heroku container:login
 docker push registry.heroku.com/fierce-depths-21005/web
-heroku container:push web -a fierce-depths-21005
 heroku container:release web -a fierce-depths-21005
