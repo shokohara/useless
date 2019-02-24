319c3a790ee0f612004d511c557f5b1bf6f534b4
shokohara <shokohara@users.noreply.github.com>
2019-02-24 22:11:23 +0900

    デプロイスクリプトを修正する

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 set -eu
+APP='jpcounderscorewww'
 cd web/front
 npm install
 npx webpack
 cd ../..
 sbt 'web/docker:publishLocal'
-docker tag web:0.1.0-SNAPSHOT registry.heroku.com/fierce-depths-21005/web
+docker tag web:0.1.0-SNAPSHOT registry.heroku.com/$APP/web
 heroku container:login
-docker push registry.heroku.com/fierce-depths-21005/web
-heroku container:release web -a fierce-depths-21005
+docker push registry.heroku.com/$APP/web
+heroku container:release web -a $APP
