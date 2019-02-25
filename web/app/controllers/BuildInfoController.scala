30b39a285f7e6c140aa7a536eaff66561d1275ee
ma-oka <satoshing236236@gmail.com>
2019-02-25 18:44:59 +0900

    wartremover.Anyを導入する

--- web/app/controllers/BuildInfoController.scala
+++ web/app/controllers/BuildInfoController.scala
@@ -10,6 +10,7 @@ import scala.concurrent.ExecutionContext
 class BuildInfoController(cc: ControllerComponents)(implicit val ec: ExecutionContext)
   extends AbstractController(cc) with Circe {
 
+  @SuppressWarnings(Array("org.wartremover.warts.Any"))
   def index = Action {
     Ok(BuildInfo.toMap.mapValues(_.toString).asJson)
   }
