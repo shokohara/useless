25221b3553ee79e729126b80e209b0c8c72b0d3b
shokohara <shokohara@users.noreply.github.com>
2019-02-25 20:54:04 +0900

    フッタを追加する

--- web/app/controllers/RootController.scala
+++ web/app/controllers/RootController.scala
@@ -1,9 +1,14 @@
 package controllers
 
+import java.time.{ZoneId, ZonedDateTime}
+
 import io.circe.java8.time._
 import io.circe.refined._
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 
 class RootController(cc: ControllerComponents) extends AbstractController(cc) {
-  def index: Action[AnyContent] = Action(Ok(views.html.Index.render()))
+
+  def index: Action[AnyContent] = Action {
+    Ok(views.html.Index.render(ZonedDateTime.now(ZoneId.of("Asia/Tokyo")).getYear))
+  }
 }
