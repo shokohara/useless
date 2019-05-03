f5f9c225181d45ecfc19474ddea6f9adff215c05
shokohara <shokohara@users.noreply.github.com>
2019-05-03 22:15:39 +0900

    テストのコンパイルエラーを修正する

--- slack/src/test/scala/com/github/shokohara/slack/HelloSpec.scala
+++ slack/src/test/scala/com/github/shokohara/slack/HelloSpec.scala
@@ -11,8 +11,8 @@ import org.scalatest.{FlatSpec, Matchers}
 import scala.util.chaining._
 
 class HelloSpec extends FlatSpec with Matchers {
+  val zoneId: ZoneId = ZoneId.of("Asia/Tokyo")
   "adtsToSummary" should "work" in {
-    val zoneId: ZoneId = ZoneId.of("Asia/Tokyo")
     val localDate = LocalDate.of(2019, 2, 15)
     val openTime = LocalTime.of(11, 31, 52)
     val afk1Time = LocalTime.of(14, 47, 33)
@@ -41,41 +41,41 @@ class HelloSpec extends FlatSpec with Matchers {
   behavior of "stringToAdt"
   "RLGURERL7".pipe { userId =>
     it should "work with open" in {
-      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T00:59:46.146700Z"), "open"))
+      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T00:59:46.146700Z"), "open"), zoneId)
     }
     it should "work with afk|qk" in {
       // このテストも通したい
 //      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-19T07:39:51.139500Z"), "qk at 07:31")) shouldEqual
 //        Afk(ZonedDateTime.parse("2019-02-19T07:31:00.000000Z")).validNec.validNec
-      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T01:36:49.147600Z"), "afk")) shouldEqual
+      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T01:36:49.147600Z"), "afk"), zoneId) shouldEqual
         Afk(ZonedDateTime.parse("2019-02-20T01:36:49.147600Z")).validNec.validNec
-      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T05:58:53.155400Z"), "qk at "))
+      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T05:58:53.155400Z"), "qk at "), zoneId)
         .map(_.leftMap(_.map(_.getMessage))) shouldEqual
         s"Message($userId,2019-02-20T05:58:53.155400Z,qk at )をcom.github.shokohara.slack.Adtに変換できません".invalidNec.validNec
-      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T07:40:53.158Z"), "qk")) shouldEqual
+      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T07:40:53.158Z"), "qk"), zoneId) shouldEqual
         Afk(ZonedDateTime.parse("2019-02-20T07:40:53.158Z")).validNec.validNec
     }
     it should "work with back" in {
-      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T09:31:53.159500Z"), "back")) shouldEqual
+      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T09:31:53.159500Z"), "back"), zoneId) shouldEqual
         Back(ZonedDateTime.parse("2019-02-20T09:31:53.159500Z")).validNec.validNec
       // このテストも通したい
 //      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T06:29:22.157800Z"), "backed at 06:21")) shouldEqual
 //        Back(ZonedDateTime.parse("2019-02-20T06:29:21.000000Z")).validNec.validNec
-      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T01:43:56.148400Z"), "bak"))
+      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T01:43:56.148400Z"), "bak"), zoneId)
         .map(_.leftMap(_.map(_.getMessage))) shouldEqual
         s"Message($userId,2019-02-20T01:43:56.148400Z,bak)をcom.github.shokohara.slack.Adtに変換できません".invalidNec.validNec
 
-      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-19T07:57:53.139700Z"), "back"))
+      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-19T07:57:53.139700Z"), "back"), zoneId)
     }
     it should "work with close" in {
       // このテストも通したい
       //    stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-19T09:59:51.141600Z"), "closed at 09:43")) shouldEqual
       //      Close(ZonedDateTime.parse("2019-02-19T09:43:00.000000")).asRight.asRight
-      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T12:02:30.164Z"), "close")) shouldEqual
+      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T12:02:30.164Z"), "close"), zoneId) shouldEqual
         Close(ZonedDateTime.parse("2019-02-20T12:02:30.164Z")).validNec.validNec
     }
     it should "work with other" in {
-      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T01:44:03.148800Z"), "猫に投薬してました :cat2:"))
+      stringToAdt(Message(userId, ZonedDateTime.parse("2019-02-20T01:44:03.148800Z"), "猫に投薬してました :cat2:"), zoneId)
         .map(_.leftMap(_.map(_.getMessage))) shouldEqual
         s"Message($userId,2019-02-20T01:44:03.148800Z,猫に投薬してました :cat2:)をcom.github.shokohara.slack.Adtに変換できません".invalidNec.validNec
     }
@@ -99,7 +99,8 @@ class HelloSpec extends FlatSpec with Matchers {
   it should """work with "open", "close PRの対応しました" """ in {
     val a: List[ValidatedNec[RuntimeException, ValidatedNec[RuntimeException, Adt]]] =
       (Message("user", ZonedDateTime.parse("2019-04-25T21:38:06.091800Z"), "open") ::
-        Message("user", ZonedDateTime.parse("2019-04-25T22:49:06.091800Z"), "close PRの対応しました") :: Nil).map(stringToAdt)
+        Message("user", ZonedDateTime.parse("2019-04-25T22:49:06.091800Z"), "close PRの対応しました") :: Nil)
+        .map(stringToAdt(_, zoneId))
     val b: NonEmptyList[Adt] = a.map(_.fold(_ => ???, _.fold(_ => ???, identity))).toNel.get
     adtsToSummary(b) shouldEqual Valid(
       Summary(
